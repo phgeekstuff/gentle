44cb4e8b8363940b703a26eaec1a43a8f59ab61c
PH GeekStuff <ph.geekstuff@gmail.com>
2017-10-19 12:40:41 +0200

    Initial structure for the code

---
+++ src/gentle.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* @brief Main entrypoint of Gentle.
+*/
+int main(int argc, char *argv[])
+{
+    int result = 0;
+
+    printf("Main entry point");
+
+    return result;
+}
