a3c01e4488cb59d9e6c434443d96273fb2b78ee9
PH GeekStuff <ph.geekstuff@gmail.com>
2017-10-19 17:10:18 +0200

    Adding files missed.

---
+++ test-sys/gentle-test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# System tests of Gentle
+#
+import unittest
+from test import support
+
+class gentleSystemTest():
+    
+    # Only use setUp() and tearDown() if necessary
+    #def setUp(self):
+
+    #def tearDown(self):
+
+    #def test_feature_one(self):
+
+    #def test_feature_two(self):
+
+    def start(self):
+        print("Testing")
+
+ # TODO: Prepare code to call gentle
+
+ # Main entry point
+ systemTest = gentleSystemTest()
+ systemTest.start()
+
+
+
